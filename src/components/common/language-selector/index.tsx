b55c33d86ea5beaf4b98aea96c76cb1991e9e1f7
Gimhan Rodrigo <gimhanrg@gmail.com>
2025-03-25 16:50:26 +0530

    disabled the language selector when pending

--- src/components/common/language-selector/index.tsx
+++ src/components/common/language-selector/index.tsx
@@ -52,7 +52,7 @@ const LanguageSelector = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild disabled={isPending}>
         <Button
           variant="ghost"
           size="sm"
