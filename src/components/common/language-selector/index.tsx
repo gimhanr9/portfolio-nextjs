4163732afd37f62d6a2de41959743da3bd298ec0
Gimhan Rodrigo <gimhanrg@gmail.com>
2025-03-25 16:40:54 +0530

    added routing when language change

--- src/components/common/language-selector/index.tsx
+++ src/components/common/language-selector/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { Check, ChevronDown, Globe } from "lucide-react";
 import {
   DropdownMenu,
@@ -11,12 +11,18 @@ import {
 import { Button } from "@/components/ui/button";
 import { LanguageSelectorType } from "./language-selector.types";
 import { siteConfig } from "@/config/site";
+import { usePathname, useRouter } from "@/i18n/routing";
+import { useParams } from "next/navigation";
 
 const LanguageSelector = () => {
   const [currentLanguage, setCurrentLanguage] =
     useState<LanguageSelectorType>();
 
   const [languages, setLanguages] = useState<Array<LanguageSelectorType>>([]);
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const params = useParams();
 
   useEffect(() => {
     let locales = siteConfig.locales;
@@ -27,6 +33,22 @@ const LanguageSelector = () => {
       );
     }
   }, []);
+
+  const handleLanguageChange = (language: LanguageSelectorType) => {
+    if (language.code === currentLanguage?.code) return;
+
+    startTransition(() => {
+      router.replace(
+        // @ts-expect-error -- TypeScript will validate that only known `params`
+        // are used in combination with a given `pathname`. Since the two will
+        // always match for the current route, we can skip runtime checks.
+        { pathname, params },
+        { locale: language.code }
+      );
+    });
+
+    setCurrentLanguage(language);
+  };
 
   return (
     <DropdownMenu>
@@ -47,7 +69,7 @@ const LanguageSelector = () => {
         {languages?.map((language) => (
           <DropdownMenuItem
             key={language.code}
-            onClick={() => setCurrentLanguage(language)}
+            onClick={() => handleLanguageChange(language)}
             className="flex items-center justify-between"
           >
             <span>
